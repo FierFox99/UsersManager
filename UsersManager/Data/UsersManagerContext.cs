9bdcfaaa8a01482cb11efb9fb03f8ff1d307f012
FierFox99 <76745898+FierFox99@users.noreply.github.com>
2021-02-15 21:47:34 -0500

    a

---
+++ UsersManager/Data/UsersManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UsersManager.Models;
+
+namespace UsersManager.Data
+{
+    public class UsersManagerContext : DbContext
+    {
+        public UsersManagerContext (DbContextOptions<UsersManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsersManager.Models.User> User { get; set; }
+    }
+}
---
+++ UsersManager/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersManager.Models
+{
+    public enum Permissions { customer, staff, admin }
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        [NotMapped]
+        public string EmailConfirm { get; set; }
+        public string Password { get; set; }
+        [NotMapped]
+        public string PasswordConfirm { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public Permissions Permissions { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
